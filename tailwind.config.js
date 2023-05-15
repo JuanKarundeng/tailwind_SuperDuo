89f811833dd932dfbd60968c81fff2cb08a1576b
Juan Karundeng <juankarundeng18@gmail.com>
2023-05-15 15:41:18 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      abu_abu: "D9D9D9",
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
